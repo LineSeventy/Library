26aa768d87322d20992c523548ea06780cc0adcd
LineSeventy <169619934+LineSeventy@users.noreply.github.com>
2024-09-10 17:19:33 +0800

    Added js

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,21 @@
-const myLibrary = [];
+const myLibrary = [{
+    "Title": 'Solo Leveling',
+    "Genre": 'Fantasy',
+    "Type": 'Webbnove',
+    "Year": 2000   
+},
+{
+    "Title": 'Reincarnator',
+    "Genre": 'Regression',
+    "Type": 'Webbnovel',
+    "Year": 2001    
+}];
 
-function Book(){
-
+function Book(title, genre , type, year){
+    this.Title = title;
+    this.Genre = genre;
+    this.Type = type;
+    this.Year = year;
 }
 
 function addBookToLibrary(){
